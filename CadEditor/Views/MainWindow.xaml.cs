df3d88f0a088eb39d9115e2d2a97c76b6e195eeb
testbest <tunawillgetit1030@gmail.com>
2026-07-10 17:12:22 +0700

    feature/mouse-drawing

--- CadEditor/Views/MainWindow.xaml.cs
+++ CadEditor/Views/MainWindow.xaml.cs
@@ -1,23 +1,57 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using CadEditor.Models;
+using CadEditor.ViewModels;
 
 namespace CadEditor.Views;
 
-/// <summary>
-/// Interaction logic for MainWindow.xaml
-/// </summary>
 public partial class MainWindow : Window
 {
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    private MainViewModel ViewModel => (MainViewModel)DataContext;
+
+    private void DrawingCanvas_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+    {
+        var pos = e.GetPosition(DrawingCanvas);
+        var point = new Point2D(pos.X, pos.Y);
+
+        if (e.ClickCount >= 2)
+        {
+            ViewModel.FinishPolygon();
+            return;
+        }
+
+        ViewModel.OnCanvasMouseDown(point);
+        DrawingCanvas.CaptureMouse();
+    }
+
+    private void DrawingCanvas_MouseMove(object sender, MouseEventArgs e)
+    {
+        var pos = e.GetPosition(DrawingCanvas);
+        ViewModel.OnCanvasMouseMove(new Point2D(pos.X, pos.Y));
+    }
+
+    private void DrawingCanvas_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+    {
+        var pos = e.GetPosition(DrawingCanvas);
+        ViewModel.OnCanvasMouseUp(new Point2D(pos.X, pos.Y));
+        DrawingCanvas.ReleaseMouseCapture();
+    }
+
+    private void Window_KeyDown(object sender, KeyEventArgs e)
+    {
+        switch (e.Key)
+        {
+            case Key.Enter:
+                ViewModel.FinishPolygon();
+                break;
+            case Key.Escape:
+                ViewModel.CancelPolygon();
+                break;
+        }
     }
 }
